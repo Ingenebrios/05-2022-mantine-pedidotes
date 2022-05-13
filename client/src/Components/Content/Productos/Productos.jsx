fecb1c8217368728a9d5d73e066e7a130f9a2747
IPablo271 <69815580+IPablo271@users.noreply.github.com>
2022-05-13 11:55:04 -0600

    Productos
    Carnes

--- client/src/Components/Content/Productos/Productos.jsx
+++ client/src/Components/Content/Productos/Productos.jsx
@@ -1,37 +1,19 @@
 import React from 'react'
 
 import { useEffect } from 'react';
-
-const carnes = [
-	{ value: 'Carnes', label: 'Carnes' },
-	{ value: 'Frutas', label: 'Frutas' },
-	{ value: 'Panaderia', label: 'Panaderia' },
-	{ value: 'Lacteos', label: 'Lacteos' },
-	{ value: 'Mariscos', label: 'Mariscos' },
-	{ value: 'Granos', label: 'Granos' },
-	{ value: 'Bebidas', label: 'Bebidas' },
-	{ value: 'Limpieza', label: 'Limpieza' },
-];
+import Democarta from './carnes';
 
 const Productos = ({ producto, categoria }) => {
-
-	useEffect(() => {
-		console.log(categoria)
-	}, [categoria]);
-
 	return (
 		<div>
 			{
-				categoria === 'carnes'
-					?
-					carnes.map(c => {
-						return (
-							<h1 key={c.label}>{c.value}</h1>
-						)
-					})
-					:
-					null
+				categoria==='carnes'
+				?
+				<Democarta/>
+				:
+				null
 			}
+
 		</div>
 	)
 }
